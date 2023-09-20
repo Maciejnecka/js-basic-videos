2441e20a7984b6f485f1148535fd1a674163fd7f
Maciej Necka <macieknecka@gmail.com>
2023-09-20 14:39:15 +0200

    5/31

---
+++ Task-0/app.js
@@ -0,0 +1,52 @@
+// // Number
+// 5;
+// 1.5;
+// // String
+// ('Hello');
+// ("I can't");
+// // Boolean
+// true;
+// false;
+// // null
+// null;
+// //undefined
+// undefined;
+
+// //object
+// ({
+//   name: 'Fluffy',
+//   age: 3,
+// })[
+//   // Array (subtype or kind of Object)
+//   ('Fluffy', 3)
+// ];
+// Functions are also Objects!!!
+// var a = 5;
+// console.log(a);
+// var b = 'Hello';
+// console.log(b);
+// b = 5;
+// console.log(b);
+
+// var numberLiteral = 5;
+// var numberLiteral2 = 1.5;
+// console.log(numberLiteral);
+// console.log(numberLiteral2);
+
+// var stringLiteral = 'Hello';
+// var stringLiteral2 = "I can't";
+// console.log(stringLiteral);
+// console.log(stringLiteral2);
+
+// var booleanLiteral = true;
+// var booleanLiteral2 = false;
+// console.log(booleanLiteral);
+// console.log(booleanLiteral2);
+
+// var nullLiteral = null;
+// var undefinedLiteral = undefined;
+// console.log(nullLiteral);
+// console.log(undefinedLiteral);
+// console.log('Hello');
+// console.log(console);
+// console.log(console.log);
